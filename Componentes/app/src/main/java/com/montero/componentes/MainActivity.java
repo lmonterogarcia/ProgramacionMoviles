aaf3d43e99685521f9e35974ae9d081ea3d333fd
Luis <lmongarman@gmail.com>
2021-10-05 14:48:53 +0200

    Practica

--- Componentes/app/src/main/java/com/montero/componentes/MainActivity.java
+++ Componentes/app/src/main/java/com/montero/componentes/MainActivity.java
@@ -3,6 +3,11 @@ package com.montero.componentes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    /*
+        FloatingActionButton btnFlotating = findViewById(R.id.btnFloatating);
+
+        btnFlotating.setOnClickListener(
+                v -> {
+                    Toast.makeText(this,"Has pulsado el flotating button",Toast.LENGTH_LONG).show();
+                    btnFloating.setVisibility(View.INVISIBLE); //Hace que desaparezca el boton
+                 }
+        );
+    */
+
+        findViewById(R.id.btnFloatating).setOnClickListener(
+                v -> {
+                    Toast.makeText(this,"Has pulsado el flotating button",Toast.LENGTH_LONG).show();
+
+                }
+        );
     }
+
 }
