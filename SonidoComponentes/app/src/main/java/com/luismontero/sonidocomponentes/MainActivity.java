5895352497c26a82aa40a4d1d203d285bee4c253
Luis <Montero>
2021-12-16 12:30:28 +0100

    musica componentes

--- SonidoComponentes/app/src/main/java/com/luismontero/sonidocomponentes/MainActivity.java
+++ SonidoComponentes/app/src/main/java/com/luismontero/sonidocomponentes/MainActivity.java
@@ -2,6 +2,8 @@ package com.luismontero.sonidocomponentes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.media.SoundPool;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SoundPool soundPandereta = new SoundPool.Builder().setMaxStreams(10).build();
+        int soundIdPandereta = soundPandereta.load(this, R.raw.pandereta, 1);
+
+        SoundPool soundZambomba = new SoundPool.Builder().setMaxStreams(10).build();
+        int soundIdZambomba = soundZambomba.load(this, R.raw.zambomba, 1);
+
+        findViewById(R.id.ivPandereta).setOnClickListener( v -> {
+            soundPandereta.play(soundIdPandereta,1,1,1,0,1);
+        });
+
+        findViewById(R.id.ivZambomba).setOnClickListener( v -> {
+            soundZambomba.play(soundIdZambomba,1,1,1,0,1);
+        });
     }
 }
