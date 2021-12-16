b5ab8ea69c1ba1b7ac1ed41522909b71a87cb5a4
Luis <Montero>
2021-12-16 11:39:27 +0100

    Terminado musica de fondo

--- MusicadeFondo/app/src/main/java/com/luismontero/musicadefondo/MainActivity.java
+++ MusicadeFondo/app/src/main/java/com/luismontero/musicadefondo/MainActivity.java
@@ -2,13 +2,45 @@ package com.luismontero.musicadefondo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer musica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        musica = MediaPlayer.create(this, R.raw.tocata);
+        //musica.start();
+
+        findViewById(R.id.btnPaly).setOnClickListener(v -> {
+            if (!musica.isPlaying()) {
+                musica.start();
+            }
+        });
+
+        findViewById(R.id.btnPause).setOnClickListener(v -> {
+            if (musica.isPlaying()) {
+                musica.pause();
+            }
+        });
+
+        findViewById(R.id.btnStop).setOnClickListener(v -> {
+            if (musica.isPlaying()) {
+                musica.stop();
+                try {
+                    musica.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                musica.seekTo(0);
+            }
+        });
     }
 }
